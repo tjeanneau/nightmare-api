a02b9c80af13eb327e6c4c66fe24f1b25b81aed5
t_jeanneau <hi@thomasjeanneau.com>
2017-03-18 20:03:37 +0100

    add express

--- index.js
+++ index.js
@@ -3,6 +3,14 @@
  */
 
 var Nightmare = require('nightmare');
+var express = require('express');
+var app = express();
+
+var server = app.listen(process.env.PORT || 3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('App listening at http://%s:%s', host, port)
+});
 
 const config = {
   show: false,
@@ -15,19 +23,22 @@ const config = {
 
 var nightmare = Nightmare(config);
 
-nightmare
-  .goto('https://www.linkedin.com/uas/login')
-  .type('#session_key-login', 'hi@thomasjeanneau.com')
-  .type('#session_password-login', 'Xf0WfnOBOPfUwrY1bXeNsGAGfAvDGjN0kuTNxrOf')
-  .click('#btn-primary')
-  .wait('#extended-nav-search')
-  .evaluate(function () {
-    return window.location.href;
-  })
-  .end()
-  .then(function (title) {
-    console.log(title === 'https://www.linkedin.com/nhome/?trk=' || title === 'https://www.linkedin.com/feed/?trk=')
-  })
-  .catch(function (err) {
-    console.log(err)
-  });
+app.get('/', function (req, res) {
+  nightmare
+    .goto('https://www.linkedin.com/uas/login')
+    .type('#session_key-login', 'hi@thomasjeanneau.com')
+    .type('#session_password-login', 'Xf0WfnOBOPfUwrY1bXeNsGAGfAvDGjN0kuTNxrOf')
+    .click('#btn-primary')
+    .wait('#extended-nav-search')
+    .evaluate(function () {
+      return window.location.href;
+    })
+    .end()
+    .then(function (title) {
+      console.log(title === 'https://www.linkedin.com/nhome/?trk=' || title === 'https://www.linkedin.com/feed/?trk=')
+    })
+    .catch(function (err) {
+      console.log(err)
+    });
+  res.send('Hello World!')
+});
